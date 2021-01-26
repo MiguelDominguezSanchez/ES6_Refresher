76815b1cf4d3921fdfd949927ea0788f01108735
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-01-26 17:50:23 +0100

    4. ES6 Refresher - Part 2

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 // CONST & LET
 
-let name = 'Jhon';
-let test;
+// let name = 'Jhon';
+// let test;
 
-name = 'Jack';
+// name = 'Jack';
 
-const person = {
-	name: 'Jhon',
-	age: 33,
-};
+// const person = {
+// 	name: 'Jhon',
+// 	age: 33,
+// };
 
-person.name = 'Jack';
+// person.name = 'Jack';
 
-const nums = [1, 2, 3, 4];
-nums.push(5);
+// const nums = [1, 2, 3, 4];
+// nums.push(5);
 
-console.log(nums);
+// console.log(nums);
 
 // ARROW FUNCTIONS
 
@@ -23,31 +23,62 @@ console.log(nums);
 // 	console.log('Hello');
 // }
 
-const sayHello = name => console.log(`Hello ${name}`);
+// const sayHello = name => console.log(`Hello ${name}`);
 
-const fruits = ['Apples', 'Oranges', 'Grapes'];
+// const fruits = ['Apples', 'Oranges', 'Grapes'];
 
 // FOREACH
 // fruits.forEach((fruit, index) => console.log(fruit));
 
 // MAP
-const singleFruit = fruits.map(fruit => fruit.slice(0, -1).toUpperCase());
+// const singleFruit = fruits.map(fruit => fruit.slice(0, -1).toUpperCase());
 
 // console.log(singleFruit);
 
 // FILTER
-const people = [
-	{ id: 1, name: 'Karen' },
-	{ id: 2, name: 'Bob' },
-	{ id: 3, name: 'Sharon' },
-];
+// const people = [
+// 	{ id: 1, name: 'Karen' },
+// 	{ id: 2, name: 'Bob' },
+// 	{ id: 3, name: 'Sharon' },
+// ];
 
-const people2 = people.filter(person => person.id !== 2);
-console.log(people2);
+// const people2 = people.filter(person => person.id !== 2);
+// console.log(people2);
 
-// SPREAD
+// // SPREAD
+// const arr = [1, 2, 3];
+// const arr2 = [...arr, 4];
+// const arr3 = [...arr.filter(num => num !== 2)];
 
-// DESTRUCTURING
+// console.log(arr3);
+
+// const person1 = {
+// 	name: 'Brad',
+// 	age: 36,
+// };
+
+// const person2 = {
+// 	...person1,
+// 	email: 'Brad@gmail.com',
+// };
+
+// console.log(person2);
+
+// // DESTRUCTURING
+
+const profile = {
+	name: 'John Doe',
+	address: {
+		street: '40 Main st',
+		city: 'Boston',
+	},
+	hobbies: ['movies', 'music'],
+};
+
+const { name, address, hobbies } = profile;
+const { street, city } = profile.address;
+
+console.log(street, city);
 
 // CLASSES
 
